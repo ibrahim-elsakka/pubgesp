5f0a797cbfc236987a05a302487429452745dfb3
raic0d3r <36988513+raic0d3r@users.noreply.github.com>
2020-10-26 02:24:46 +0100

    Update dnscacheclean.bat

--- dnscacheclean.bat
+++ dnscacheclean.bat
@@ -1,8 +1,14 @@
-@echo off
-netsh advfirewall reset
-netsh winsock reset
-netsh int ip reset
+netsh interface ip show config
+ipconfig /all
+ipconfig /registerdns
+ipconfig /displaydns
+ipconfig /release
 ipconfig /renew
-
-Timeout 19 
 ipconfig /flushdns
+netsh int ip reset
+netsh winsock reset
+netsh interface ipv4 reset
+netsh interface ipv6 reset
+netsh advfirewall reset
+nbtstat -r
+nbtstat -rr
