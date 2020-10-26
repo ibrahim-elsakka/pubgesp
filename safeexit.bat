f2a754f164b26f2337045a8bbc2be93c64c03040
raic0d3r <36988513+raic0d3r@users.noreply.github.com>
2020-10-26 02:10:34 +0100

    Update safeexit.bat

--- safeexit.bat
+++ safeexit.bat
@@ -34,3 +34,17 @@ net stop Tensafe
 netsh advfirewall set publicprofile state on
 netsh advfirewall set domainprofile state on
 netsh advfirewall set privateprofile state on
+netsh interface ip show config
+ipconfig /all
+ipconfig /registerdns
+ipconfig /displaydns
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh int ip reset
+netsh winsock reset
+netsh interface ipv4 reset
+netsh interface ipv6 reset
+netsh advfirewall reset
+nbtstat -r
+nbtstat -rr
